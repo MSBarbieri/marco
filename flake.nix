c209845516d7b1ff4cc1a403d0393b2a36208f7b
MSBarbieri <matheussouzabarbieri@gmail.com>
2023-03-30 13:45:15 -0300

    separate projects

--- flake.nix
+++ flake.nix
@@ -22,16 +22,38 @@
     let
       pkgs = import nixpkgs {
         inherit system;
+        allowUnfree = true;
         overlays = [
           fenix.overlays.default
         ];
       };
-      naersk-lib = pkgs.callPackage naersk { };
+
+      toolchain = with fenix.packages.${system}; combine [
+        complete.cargo
+        complete.rustc
+        complete.rustfmt
+        complete.rust-src
+        complete.clippy
+        # targets.x86_64-linux.latest.rust-std
+        targets.wasm32-unknown-unknown.latest.rust-std
+      ];
+
+      naersk-lib = (naersk.lib.${system}.override {
+        cargo = toolchain;
+        rustc = toolchain;
+      });
     in
     {
       defaultPackage = naersk-lib.buildPackage {
         src = ./.;
-        buildInputs = [ ];
+
+        nativeBuildInputs = [ pkgs.pkg-config ];
+        buildInputs = with pkgs; [
+          wasm-pack
+          openssl
+          protobuf
+          pkg-config
+        ];
         RUST_LOG = "trace";
       };
       checks = {
@@ -53,23 +75,21 @@
         inherit (self.checks.${system}.pre-commit-check) shellHook;
 
         buildInputs = with pkgs; [
-          (pkgs.fenix.complete.withComponents [
-            "cargo"
-            "clippy"
-            "rust-src"
-            "rustc"
-            "rustfmt"
-          ])
+          toolchain
           cargo-watch
+          nodejs-16_x
           pre-commit
           nixpkgs-fmt
           openssl
           protobuf
+          wasm-pack
         ];
 
         RUST_LOG = "debug";
 
-        nativeBuildInputs = [ pkgs.pkg-config ];
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+        ];
         RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
       };
     });
